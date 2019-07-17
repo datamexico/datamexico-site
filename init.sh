bd7d499735f4882588ef2f66a83be975c907e32e
cnavarreteliz <cnavarreteliz@gmail.com>
2019-07-17 18:00:30 -0400

    includes init script for front-end

---
+++ init.sh
@@ -0,0 +1,6 @@
+rm -rf node_modules/ package-lock.json
+npm i
+CANON_CONST_BASE='https://api.datamexico.org/tesseract/' node --max_old_space_size=6000 `which npm` run build
+pm2 kill
+pm2 restart ecosystem.config.js --update-env
+pm2 save
