2fb8aa7a4a3bc77a52e0dd2034ea5b555da14baf
cnavarreteliz <cnavarreteliz@gmail.com>
2019-08-28 11:07:46 -0400

    forces canon-version

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -3,20 +3,47 @@ import {withNamespaces} from "react-i18next";
 import {Icon, InputGroup, Popover} from "@blueprintjs/core";
 import NavMenu from "./NavMenu";
 import classnames from "classnames";
+import axios from "axios";
 
 
 import "./Nav.css";
+import SearchResult from "./SearchResult";
 
 
 class Nav extends React.Component {
   state = {
     isOpen: false,
-    isSearchOpen: false
+    isSearchOpen: false,
+    results: [],
+    resultsFilter: [],
+    isOpenSearchResults: false
+  }
+
+  handleSearch = e => {
+    const {results} = this.state;
+    const query = e.target.value;
+
+    if (query.length > 0) {
+      axios.get(`/api/search?q=${query}`).then(resp => {
+        const data = resp.data.results;
+        const results = data.map(d => ({id: d.id, name: d.name, slug: d.profile, level: d.hierarchy}));
+        this.setState({results, resultsFilter: results});
+      });
+    }
+
+    const resultsFilter = query.length > 0
+      ? results.filter(d => d.name.toLowerCase().indexOf(query.toLowerCase()) >= 0)
+      : [];
+
+    const isOpen = query.length > 2;
+    this.setState({resultsFilter, isOpenSearchResults: isOpen});
   }
 
   render() {
     const {className, logo, title, t} = this.props;
-    const {isOpen, isSearchOpen} = this.state;
+    const {isOpen, isSearchOpen, resultsFilter} = this.state;
+
+    console.log(this.state);
 
     return <div className={`${className} nav click`}>
       <NavMenu
@@ -34,23 +61,21 @@ class Nav extends React.Component {
       <div className="nav-right">
         <div className={classnames("search-button", {active: isSearchOpen})}>
           <Icon icon="search" className="click" onClick={() => this.setState({isSearchOpen: !isSearchOpen})} />
-          <Popover
-            popoverClassName="nav-search-popover"
-            content={<div className="">
-              hola
-            </div>}
-            portal={true}
-            portalClassName="nav-search-portal"
-            position="bottom"
-            minimal={true}
-            isOpen={isSearchOpen}
-          >
-            <InputGroup
-              placeholder={t("Search profiles")}
-              className={classnames({active: isSearchOpen})}
-              autoFocus="true"
-            />
-          </Popover>
+          <InputGroup
+            placeholder={t("Search profiles")}
+            className={classnames({active: isSearchOpen})}
+            autoFocus="true"
+            onChange={this.handleSearch}
+          />
+          <ul className={classnames("results", {active: isSearchOpen})}>
+            {resultsFilter.map((d, i) => <SearchResult
+              key={`search_result_${d.id}_${i}`}
+              id={d.id}
+              slug={d.slug}
+              title={d.name}
+              level={d.level}
+            />)}
+          </ul>
         </div>
       </div>
     </div>;
