249478d74253bebec77e09ccfeff665e61230307
cnavarreteliz <cnavarreteliz@gmail.com>
2019-09-23 11:59:11 -0300

    updates loading style

--- app/routes.jsx
+++ app/routes.jsx
@@ -5,6 +5,7 @@ import {Builder} from "@datawheel/canon-cms";
 import App from "./App";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Loading from "./components/Loading";
 
 /** */
 export default function RouteCreate() {
@@ -12,6 +13,7 @@ export default function RouteCreate() {
     <Route path="/" component={App} history={browserHistory}>
       <IndexRoute component={Home} />
       <Route path="/admin" component={Builder} />
+      <Route path="/loading" component={Loading} />
       <Route exact path="/:lang/profile/:slug/:id" component={Profile} />
     </Route>
   );
