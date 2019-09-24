55b5f8cef7e22332e55afff6473db28ce061e18d
cnavarreteliz <cnavarreteliz@gmail.com>
2019-09-24 09:15:57 -0300

    updates init script

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+git fetch -p && git pull
 rm -rf node_modules/ package-lock.json
 npm i
 CANON_CONST_BASE='https://api.datamexico.org/tesseract/data' node --max_old_space_size=6000 `which npm` run build
