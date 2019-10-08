b0b8b1f12af0a62b897e6eebab088ee2f4e7777c
cnavarreteliz <cnavarreteliz@gmail.com>
2019-10-08 10:45:20 -0300

    updates relatedness endpoint

--- api/complexityRoute.js
+++ api/complexityRoute.js
@@ -29,7 +29,34 @@ module.exports = function(app) {
     });
   });
 
-  app.get("/api/density", (req, res) => {
+
+  app.get("/api/rca", (req, res) => {
+    const pyFilePath = path.join(__dirname, "..", "calc/testrca.py");
+    const py = spawn(
+      "python3",
+      ["-W", "ignore", pyFilePath, JSON.stringify(req.query), "https://api.datamexico.org/tesseract/data"]
+    );
+    let respString = "";
+
+    // build response string based on results of python script
+    py.stdout.on("data", data => respString += data.toString());
+    // catch errors
+    py.stderr.on("data", data => console.error(`\nstderr:\n${data}`));
+    // return response
+    py.stdout.on("end", () => {
+      try {
+        const dataResult = JSON.parse(respString);
+        return res.json(dataResult);
+      }
+      catch (e) {
+        console.error(`Caught Error in ${"entityType"} Prediciton JSON conversion:\n${e}`);
+        console.log(`\nrespString:\n${respString}`);
+        return res.json({error: e});
+      }
+    });
+  });
+
+  app.get("/api/relatedness", (req, res) => {
     const pyFilePath = path.join(__dirname, "..", "calc/test.py");
     const py = spawn(
       "python3",
