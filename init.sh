dcc65c16bb1d8379353dc57be07a2b608eff6d7b
cnavarreteliz <cnavarreteliz@gmail.com>
2019-10-17 15:06:33 -0300

    updates ingestion script

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-git fetch -p && git pull
 rm -rf node_modules/ package-lock.json
+git fetch -p && git pull
 npm i
 CANON_CONST_BASE='https://api.datamexico.org/tesseract/data' node --max_old_space_size=6000 `which npm` run build
 pm2 kill
