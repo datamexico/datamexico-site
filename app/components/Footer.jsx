a830f0d2f7ef6f6b94f45b9e48d0bf36ed1e1bfb
James Ferrell <hello@jamesferrell.me>
2019-11-01 17:41:09 -0400

    cleans up footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -8,55 +8,56 @@ class Footer extends React.Component {
   render() {
     const {t} = this.props;
 
-    return <footer id="Footer" className="footer">
-      <div className="container">
-        <div className="columns">
-          <div className="column">
+    return <footer className="footer container">
+      <div className="columns">
+        <div className="column">
 
-            <div className="footer-links">
-              <div className="explore-columns">
-                <div className="explore-column">
-                  <h4>{t("Explore")}</h4>
-                  <ul>
-                    <li>{t("Profiles")}</li>
-                  </ul>
-                </div>
-                <div className="explore-column">
-                  <h4>{t("Sources")}</h4>
-                  <ul>
-                    <li>{t("Data Sources")}</li>
-                    <li>{t("API")}</li>
-                    <li>{t("Classifications")}</li>
-                    <li>{t("Contact us")}</li>
-                  </ul>
-                </div>
-                <div className="explore-column">
-                  <h4>{t("About")}</h4>
-                  <ul>
-                    <li>{t("Background")}</li>
-                    <li>{t("In the press")}</li>
-                    <li>{t("Team")}</li>
-                    <li>{t("Glossary")}</li>
-                    <li>{t("Terms of use")}</li>
-                  </ul>
-                </div>
+          <div className="footer-links">
+            <div className="footer-columns">
+              <div className="footer-column">
+                <h2 className="u-visually-hidden">Site navigation</h2>
+                <h3 className="footer-heading display u-font-sm">{t("Explore")}</h3>
+                <ul>
+                  <li><a href="/explore">{t("Profiles")}</a></li>
+                  <li><a href="#">{t("Viz Builder")}</a></li>
+                  <li><a href="#">{t("Data Cart")}</a></li>
+                </ul>
+              </div>
+              <div className="footer-column">
+                <h3 className="footer-heading display u-font-sm">{t("Sources")}</h3>
+                <ul>
+                  <li><a href="#">{t("Data Sources")}</a></li>
+                  <li><a href="#">{t("API")}</a></li>
+                  <li><a href="#">{t("Classifications")}</a></li>
+                  <li><a href="#">{t("Contact us")}</a></li>
+                </ul>
+              </div>
+              <div className="footer-column">
+                <h3 className="footer-heading display u-font-sm">{t("About")}</h3>
+                <ul>
+                  <li><a href="#">{t("Background")}</a></li>
+                  <li><a href="#">{t("In the press")}</a></li>
+                  <li><a href="#">{t("Team")}</a></li>
+                  <li><a href="#">{t("Glossary")}</a></li>
+                  <li><a href="#">{t("Terms of use")}</a></li>
+                </ul>
               </div>
             </div>
-
           </div>
-          <div className="column footer-contact">
-            <h4>{t("Receive updates on news, datasets, and features?")}</h4>
-            <InputGroup
-              leftIcon="envelope"
-              className="footer-email"
-              placeholder="Your email address"
-              rightElement={<span>{t("Sign In")}<Icon icon="arrow-right" /></span>}
-            />
-            <div className="sponsors">
-              <img className="brand" src="/icons/SE.png" alt="" />
-              <img className="brand" src="/icons/matt-white.svg" alt="" />
-              <img className="brand" src="/icons/datawheel-white.svg" alt="" />
-            </div>
+
+        </div>
+        <div className="column footer-contact">
+          <h3 className="u-font-sm">{t("Receive updates on news, datasets, and features?")}</h3>
+          <InputGroup
+            leftIcon="envelope"
+            className="footer-email"
+            placeholder="Your email address"
+            rightElement={<span>{t("Sign Up")}<Icon icon="arrow-right" /></span>}
+          />
+          <div className="sponsors">
+            <img className="brand" src="/icons/SE.png" alt="" />
+            <img className="brand" src="/icons/matt-white.svg" alt="" />
+            <img className="brand" src="/icons/datawheel-white.svg" alt="" />
           </div>
         </div>
       </div>
