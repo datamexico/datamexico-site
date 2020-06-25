eb54db1cfc978200d525861633a793f0a18c7173
nicoperezmundaca <nico.perezmundaca@gmail.com>
2020-06-25 18:38:10 -0400

    add colors and delete preview stats

--- app/pages/Covid/Covid.jsx
+++ app/pages/Covid/Covid.jsx
@@ -1,36 +1,35 @@
-import React, {Component} from "react";
 import PropTypes from "prop-types";
+import React, {Component} from "react";
 import axios from "axios";
 import classnames from "classnames";
 import {Helmet} from "react-helmet";
 import {withNamespaces} from "react-i18next";
-import {arrayLengthCompare} from "@blueprintjs/core/lib/esm/common/utils";
+
+import colors from "../../../static/data/colors.json";
+import {commas} from "helpers/utils";
+import {weekdaysNames, monthsNames} from "helpers/helpers";
+
+import CovidCard from "components/CovidCard";
+import CovidTable from "components/CovidTable";
+import DMXPreviewStats from "components/DMXPreviewStats";
+import DMXSearchLocation from "components/DMXSearchLocation";
+import DMXSelectLocation from "components/DMXSelectLocation";
+import Footer from "components/Footer";
+import Loading from "components/Loading";
+import Nav from "components/Nav";
 
 import "./Covid.css";
-
-import {weekdaysNames, monthsNames} from "../../helpers/helpers";
-import {commas} from "helpers/utils";
-import colors from "../../../static/data/colors.json";
-
-import Nav from "../../components/Nav";
-import Footer from "../../components/Footer";
-import Loading from "../../components/Loading";
-import CovidCard from "../../components/CovidCard";
-import CovidTable from "components/CovidTable";
-import DMXSearchLocation from "../../components/DMXSearchLocation";
-import DMXSelectLocation from "../../components/DMXSelectLocation";
-import DMXPreviewStats from "../../components/DMXPreviewStats";
 
 class Covid extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      locations: null,
+      _dataLoaded: false,
       dataActual: null,
       dataHistorical: null,
+      locationArray: null,
       locationBase: null,
-      locationsSelected: [],
-      _dataLoaded: false
+      locationSelected: []
     };
     this.selectNewLocation = this.selectNewLocation.bind(this);
     this.addNewLocation = this.addNewLocation.bind(this);
@@ -51,26 +50,33 @@ class Covid extends Component {
 
   fetchData = () => {
     axios.get("/api/covid").then(resp => {
+      const data = resp.data;
+      const dataActual = data.data_actual;
+      const dataHistorical = data.data_historical;
+      const locationArray = data.locations;
+      const locationBase = locationArray[0];
+      const locationSelected = [locationBase["Location ID"]];
       this.setState({
-        locations: resp.data.locations,
-        dataActual: resp.data.data_actual,
-        dataHistorical: resp.data.data_historical,
-        locationBase: resp.data.locations[0],
-        locationsSelected: [resp.data.locations[0]["Location ID"]],
-        _dataLoaded: true
+        _dataLoaded: true,
+        dataActual,
+        dataHistorical,
+        locationArray,
+        locationBase,
+        locationSelected
       })
     });
   }
 
+  // add these two in to one function
   selectNewLocation = (location) => {
-    const {locationBase, locationsSelected} = this.state;
-    if (!locationsSelected.includes(location["Location ID"])) {
-      locationsSelected.push(location["Location ID"]);
-      const index = locationsSelected.indexOf(locationBase["Location ID"]);
-      locationsSelected.splice(index, 1);
+    const {locationBase, locationSelected} = this.state;
+    if (!locationSelected.includes(location["Location ID"])) {
+      locationSelected.push(location["Location ID"]);
+      const index = locationSelected.indexOf(locationBase["Location ID"]);
+      locationSelected.splice(index, 1);
       this.setState({
         locationBase: location,
-        locationsSelected: locationsSelected
+        locationSelected: locationSelected
       });
     } else {
       this.setState({
@@ -80,115 +86,128 @@ class Covid extends Component {
   }
 
   addNewLocation = (location, event) => {
-    const {locationsSelected} = this.state;
+    const {locationSelected} = this.state;
     if (event) {
-      locationsSelected.push(location["Location ID"]);
+      locationSelected.push(location["Location ID"]);
     } else {
-      const index = locationsSelected.indexOf(location["Location ID"]);
-      locationsSelected.splice(index, 1);
+      const index = locationSelected.indexOf(location["Location ID"]);
+      locationSelected.splice(index, 1);
     }
     this.setState({
-      locationsSelected: locationsSelected
+      locationSelected: locationSelected
     });
   }
 
   render() {
+    const {
+      _dataLoaded,
+      dataActual,
+      dataHistorical,
+      locationArray,
+      locationBase,
+      locationSelected
+    } = this.state;
     const {t} = this.props;
-    const {locations, dataActual, dataHistorical, locationBase, locationsSelected, _dataLoaded} = this.state;
 
-    if (_dataLoaded) {
-      const baseData = dataActual.find(d => d["Location ID"] === locationBase["Location ID"]);
-      const updateDate = new Date(dataActual[0].Time);
-      const dataUpdateDate = {
-        Day: weekdaysNames[updateDate.getDay()],
-        Number: updateDate.getDate(),
-        Month: monthsNames[updateDate.getMonth()],
-        Year: updateDate.getFullYear()
-      };
+    if (!_dataLoaded) return <Loading />;
 
-      return <div className="covid-wrapper">
-        <Helmet title="Coronavirus">
-          <meta property="og:title" content={"Coronavirus"} />
-        </Helmet>
-        <Nav
-          className={"background"}
-          logo={false}
-          routeParams={this.props.router.params}
-          routePath={"/:lang"}
-          title={""}
-        />
-        <div className="covid-site">
-          <div className="covid-header">
-            <DMXSearchLocation
-              locationOptions={locations}
-              locationSelected={locationBase}
-              selectNewLocation={this.selectNewLocation}
-            />
-            <h4 className="covid-header-date">{`Data actualizada al ${dataUpdateDate.Number + 1} ${dataUpdateDate.Month} ${dataUpdateDate.Year}`}</h4>
-            <DMXPreviewStats
-              data={baseData}
-              stats={[
-                {ID: "Daily Cases", Name: "Nuevos Casos", IconName: "nuevo-caso-icon.svg"},
-                {ID: "Daily Deaths", Name: "Nuevas Muertes", IconName: "nueva-muerte-icon.svg"},
-                {ID: "Cases last 7 Days", Name: "Casos Última Semana", IconName: "casos-ultima-semana-icon.svg"},
-                {ID: "Deaths last 7 Days", Name: "Muertes Última Semana", IconName: "muertes-ultima-semana-icon.svg"},
-                {ID: "Accum Cases", Name: "Casos Confirmados", IconName: "casos-confirmados-icon.svg"},
-                {ID: "Accum Deaths", Name: "Muertes Confirmadas", IconName: "muertes-confirmadas-icon.svg"}
-              ]}
-            />
+    const locationData = dataActual.find(d => d["Location ID"] === locationBase["Location ID"]);
+    const locationStats = [
+      {id: "stat_new_cases", name: "Nuevos Casos", icon: "nuevo-caso-icon.svg", value: commas(locationData["Daily Cases"])},
+      {id: "stat_new_dead", name: "Nuevas Muertes", icon: "nueva-muerte-icon.svg", value: commas(locationData["Daily Deaths"])},
+      {id: "stat_lastweek_cases", name: "Casos Última Semana", icon: "casos-ultima-semana-icon.svg", value: commas(locationData["Cases last 7 Days"])},
+      {id: "stat_lastweek_dead", name: "Muertes Última Semana", icon: "muertes-ultima-semana-icon.svg", value: commas(locationData["Deaths last 7 Days"])},
+      {id: "stat_accum_cases", name: "Casos Confirmados", icon: "casos-confirmados-icon.svg", value: commas(locationData["Accum Cases"])},
+      {id: "stat_accum_dead", name: "Muertes Confirmadas", icon: "muertes-confirmadas-icon.svg", value: commas(locationData["Accum Deaths"])}
+    ];
+    const updateDate = new Date(dataActual[0].Time);
+    const dataUpdateDate = {
+      Day: weekdaysNames[updateDate.getDay()],
+      Number: updateDate.getDate(),
+      Month: monthsNames[updateDate.getMonth()],
+      Year: updateDate.getFullYear()
+    };
+
+    return <div className="covid-wrapper">
+      <Helmet title="Coronavirus">
+        <meta property="og:title" content={"Coronavirus"} />
+      </Helmet>
+      <Nav
+        className={"background"}
+        logo={false}
+        routeParams={this.props.router.params}
+        routePath={"/:lang"}
+        title={""}
+      />
+      <div className="covid-site">
+        <div className="covid-header">
+          <DMXSearchLocation
+            locationOptions={locationArray}
+            locationSelected={locationBase}
+            selectNewLocation={this.selectNewLocation}
+          />
+          <h4 className="covid-header-date">{`Data actualizada al ${dataUpdateDate.Number + 1} ${dataUpdateDate.Month} ${dataUpdateDate.Year}`}</h4>
+          <div className="covid-header-stats">
+            {locationStats.map(d => (
+              <div className="covid-header-stats-stat">
+                <img src={`/icons/visualizations/covid/${d.icon}`} alt="" className="covid-header-stats-stat-icon" />
+                <div className="covid-header-stats-stat-text">
+                  <span className="covid-header-stats-stat-value">{d.value}</span>
+                  <span className="covid-header-stats-stat-name">{d.name}</span>
+                </div>
+              </div>
+            ))}
           </div>
-          <div className="covid-body container">
-            <CovidCard
-              cardTitle={"Nuevos casos diarios"}
-              cardDescription={<p>
-                Las pruebas y los desafíos limitados en la atribución de la causa de la muerte signifca que el número de muertes confrmadas puede no ser un recuento exacto del número verdadero de muertes por COVID-19.
+        </div>
+        <div className="covid-body container">
+          <CovidCard
+            cardTitle={"Nuevos casos diarios"}
+            cardDescription={<p>
+              Las pruebas y los desafíos limitados en la atribución de la causa de la muerte signifca que el número de muertes confrmadas puede no ser un recuento exacto del número verdadero de muertes por COVID-19.
               </p>}
-              data={dataHistorical}
-              dataSource={"Datos proveídos por el Gobierno de México."}
-              dataLimit={14}
-              locationsSelected={locationsSelected}
-              locationsSelector={
-                <DMXSelectLocation
-                  locationBase={locationBase}
-                  locationsOptions={locations}
-                  locationsSelected={locationsSelected}
-                  addNewLocation={this.addNewLocation}
-                />
-              }
-              scaleSelector={[
-                {name: t("Lineal"), id: "linear"},
-                {name: t("Logarítmica"), id: "log"}
-              ]}
-              indicatorSelector={[
-                {name: "Casos Diarios", id: "Daily Cases"},
-                {name: "Casos Confirmados", id: "Accum Cases"},
-                {name: "Muertes", id: "Accum Deaths"}
-              ]}
-              visualization={{
-                type: "LinePlot",
-                config: {
-                  groupBy: "Location ID",
-                  height: 400,
-                  lineLabels: true,
-                  x: "Time",
-                  time: "Time",
-                  timeline: false,
-                  shapeConfig: {
-                    Line: {
-                      stroke: d => colors.State[d["Location ID"]] || "#235B4E"
-                    }
+            data={dataHistorical}
+            dataSource={"Datos proveídos por el Gobierno de México."}
+            dataLimit={14}
+            locationsSelected={locationSelected}
+            locationsSelector={
+              <DMXSelectLocation
+                locationBase={locationBase}
+                locationsOptions={locationArray}
+                locationsSelected={locationSelected}
+                addNewLocation={this.addNewLocation}
+              />
+            }
+            scaleSelector={[
+              {name: t("Lineal"), id: "linear"},
+              {name: t("Logarítmica"), id: "log"}
+            ]}
+            indicatorSelector={[
+              {name: "Casos Diarios", id: "Daily Cases"},
+              {name: "Casos Confirmados", id: "Accum Cases"},
+              {name: "Muertes", id: "Accum Deaths"}
+            ]}
+            visualization={{
+              type: "LinePlot",
+              config: {
+                groupBy: "Location ID",
+                height: 400,
+                lineLabels: true,
+                x: "Time",
+                time: "Time",
+                timeline: false,
+                shapeConfig: {
+                  Line: {
+                    stroke: d => colors.State[d["Location ID"]] || "#235B4E"
                   }
                 }
-              }}
-            />
-          </div>
-          <CovidTable />
+              }
+            }}
+          />
         </div>
-        <Footer />
-      </div>;
-    } else {
-      return <Loading />
-    }
+        <CovidTable />
+      </div>
+      <Footer />
+    </div>;
   }
 }
 
