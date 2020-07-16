58e28ac8b37dc032dde0ce4b3716454a3f8a082e
cnavarreteliz <cnavarreteliz@gmail.com>
2020-07-16 12:46:30 -0400

    updates footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,9 +3,59 @@ import {withNamespaces} from "react-i18next";
 import {Icon, InputGroup} from "@blueprintjs/core";
 
 import {FOOTER_NAV, LOGOS} from "helpers/consts.js";
+import {Toaster, Intent, Position} from "@blueprintjs/core";
+import axios from "axios";
+
 import "./Footer.css";
 
 class Footer extends React.Component {
+
+  state = {
+    email: ""
+  };
+
+  refHandlers = {
+    toaster: ref => this.toaster = ref
+  };
+
+  addToast = toast => {
+    toast.timeout = 5000;
+    if (!toast.intent) {
+      toast.className = "toast-success";
+      toast.intent = Intent.SUCCESS;
+    }
+    this.toaster.show(toast);
+  }
+
+  handleUser = evt => {
+    const {email} = this.state;
+
+    const formData = new FormData();
+
+    formData.append("entry.1182831599", email); // Email
+    // Reset input content
+    evt.target.reset();
+    evt.preventDefault();
+
+    const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+    const GOOGLE_FORM_ACTION_URL =
+      "https://docs.google.com/forms/d/e/1FAIpQLSexPJnu7z2NEesS2o4dwpFHp8BAxjtCeWbU0FKvyOXw91y0ag/formResponse";
+
+    axios
+      .post(CORS_PROXY + GOOGLE_FORM_ACTION_URL, formData)
+      .then(() => {
+        this.addToast({
+          message: "Gracias por suscribirte en DataMéxico."
+        });
+      })
+      .catch(() => {
+        this.addToast({
+          intent: Intent.DANGER,
+          message: "Algo sucedió mal. Intente nuevamente"
+        });
+      });
+  };
+
   render() {
     const {t} = this.props;
 
@@ -34,12 +84,16 @@ class Footer extends React.Component {
         </div>
         <div className="column footer-contact">
           <h3 className="u-font-sm">{t("Receive updates on news, datasets, and features?")}</h3>
-          <InputGroup
-            leftIcon="envelope"
-            className="footer-email"
-            placeholder={t("Your email address")}
-            rightElement={<button className="submit-button" onClick={() => console.log("hook me up carlos!")}>{t("Subscribe me")}<Icon icon="arrow-right" /></button>}
-          />
+          <form action="" onSubmit={this.handleUser}>
+            <InputGroup
+              leftIcon="envelope"
+              className="footer-email"
+              type="email"
+              placeholder={t("Your email address")}
+              onChange={evt => this.setState({email: evt.target.value})}
+              rightElement={<button className="submit-button">{t("Subscribe me")}<Icon icon="arrow-right" /></button>}
+            />
+          </form>
 
           {/* _gotta_ have them logos */}
           <div className="footer-logo-list" role="contentinfo">
@@ -51,6 +105,13 @@ class Footer extends React.Component {
           </div>
         </div>
       </div>
+
+      <Toaster position={Position.BOTTOM} ref={this.refHandlers.toaster}>
+        {/* <Toast
+          intent={Intent.DANGER}
+          // message="No pudimos almacenar tu respuesta. Inténtalo más tarde."
+        /> */}
+      </Toaster>
     </footer>;
   }
 }
