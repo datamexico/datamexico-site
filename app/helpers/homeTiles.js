a4989758a3fe0b27d45fb68c7fda69ad2c7dbfbb
ffigueroal <felipe.figueroa.lara@gmail.com>
2020-07-16 13:50:32 -0400

    add subtitles to homepages tiles groups

--- app/helpers/homeTiles.js
+++ app/helpers/homeTiles.js
@@ -3,30 +3,60 @@ module.exports = {
     name: "Cities & Places",
     dimension: "Geography",
     background: "#8b9f65",
-    items: [1, 9007, 13, 9, 32001]
+    items: [1, 9007, 13, 9, 32001],
+    subtitle: "Más poblados",
+    levels: [
+      {name: "Nation", count: 1},
+      {name: "State", count: 32},
+      {name: "Municipality", count: 32}
+    ]
   },
   industry: {
     dimension: "Industry",
     background: "#f5c094",
     items: [6221, 5611, 6111, 2122, 8111],
-    name: "Industries"
+    name: "Industries",
+    subtitle: "Más productivas",
+    levels: [
+      {name: "Nation", count: 1},
+      {name: "State", count: 32},
+      {name: "Municipality", count: 32}
+    ]
   },
   occupation: {
     dimension: "Occupation Actual Job",
     background: "#68adcd",
     items: [2332, 2253, 4211, 9111, 2412],
-    name: "Occupations"
+    name: "Occupations",
+    subtitle: "1er semestre 2020",
+    levels: [
+      {name: "Nation", count: 1},
+      {name: "State", count: 32},
+      {name: "Municipality", count: 32}
+    ]
   },
   product: {
     dimension: "Product",
     background: "#ea8db2",
     items: [178703, 20804, 52603, 168504, 209401],
-    name: "Products"
+    name: "Products",
+    subtitle: "Más exportados",
+    levels: [
+      {name: "Nation", count: 1},
+      {name: "State", count: 32},
+      {name: "Municipality", count: 32}
+    ]
   },
   institution: {
     dimension: "Campus",
     background: "#e7d98c",
     items: [317, 248, 683, 725, 82],
-    name: "Universities"
+    name: "Universities",
+    subtitle: "QS Ranking",
+    levels: [
+      {name: "Nation", count: 1},
+      {name: "State", count: 32},
+      {name: "Municipality", count: 32}
+    ]
   }
 };
