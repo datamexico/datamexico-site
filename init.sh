d32cfbd7d767ac7cb07566b154f74f0e90b43c2f
cnavarreteliz <cnavarreteliz@gmail.com>
2020-08-03 17:46:38 -0400

    updates init script

--- init.sh
+++ init.sh
@@ -2,7 +2,6 @@ rm -rf node_modules/
 git fetch -p
 git pull
 npm ci
-CANON_CONST_BASE='https://api.datamexico.org/tesseract/data' npm run build
-pm2 kill
+npm run build
 pm2 restart ecosystem.config.js --update-env
 pm2 save
