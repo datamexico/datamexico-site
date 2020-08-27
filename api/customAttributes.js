75d2759d834f143237133289f5e986d2d3316a72
Annabella Zapata <annabellaizm@gmail.com>
2020-08-27 11:11:13 -0400

    Fix code formater

--- api/customAttributes.js
+++ api/customAttributes.js
@@ -1,18 +1,18 @@
 const axios = require("axios");
 const yn = require("yn");
-const { CANON_CMS_CUBES } = process.env;
+const {CANON_CMS_CUBES} = process.env;
 const verbose = yn(process.env.CANON_CMS_LOGGING);
 const BASE_API = `${CANON_CMS_CUBES}data.jsonrecords`;
 
-const catcher = (error) => {
+const catcher = error => {
   if (verbose) console.error("Custom Attribute Error:", error);
   return [];
 };
-module.exports = function (app) {
-  app.post("/api/cms/customAttributes/:pid", async (req, res) => {
+module.exports = function(app) {
+  app.post("/api/cms/customAttributes/:pid", async(req, res) => {
     const pid = req.params.pid * 1;
-    const { cache } = app.settings;
-    const { variables, locale } = req.body;
+    const {cache} = app.settings;
+    const {variables} = req.body;
     const {
       id1,
       dimension1,
@@ -20,53 +20,53 @@ module.exports = function (app) {
       slug1,
       name1,
       cubeName1,
-      parents1,
+      parents1
     } = variables;
 
     // ENOE: Shared customAttribute
-    const ENOE_DATASET = async (hierarchy, id) => {
+    const ENOE_DATASET = async(hierarchy, id) => {
       const params = {
         cube: "inegi_enoe",
         drilldowns: "Quarter",
         measures: "Workforce",
-        [hierarchy]: id,
+        [hierarchy]: id
       };
       const data = await axios
-        .get(BASE_API, { params })
-        .then((resp) => resp.data.data)
+        .get(BASE_API, {params})
+        .then(resp => resp.data.data)
         .catch(catcher);
 
       data.sort((a, b) => b["Quarter ID"] - a["Quarter ID"]);
       const enoeLatestQuarter = data[0] ? data[0]["Quarter ID"] : undefined;
       const enoePrevQuarter = data[1] ? data[1]["Quarter ID"] : undefined;
       const enoePrevYear = data[4] ? data[4]["Quarter ID"] : undefined;
-      return { enoeLatestQuarter, enoePrevQuarter, enoePrevYear };
+      return {enoeLatestQuarter, enoePrevQuarter, enoePrevYear};
     };
 
     // FDI: Shared customAttribute
-    const FDI_DATASET = async (hierarchy, id) => {
+    const FDI_DATASET = async(hierarchy, id) => {
       const params = {
         cube: "economy_fdi",
         drilldowns: "Quarter",
         measures: "Investment (Million)",
         parents: true,
-        [hierarchy]: id,
+        [hierarchy]: id
       };
       const data = await axios
-        .get(BASE_API, { params })
-        .then((resp) => resp.data.data)
+        .get(BASE_API, {params})
+        .then(resp => resp.data.data)
         .catch(catcher);
 
       data.sort((a, b) => b["Quarter ID"] - a["Quarter ID"]);
       const fdiLatestQuarter = data[0] ? data[0]["Quarter ID"] : undefined;
-      const fdiLatestYear = data[0] ? data[0]["Year"] : undefined;
+      const fdiLatestYear = data[0] ? data[0].Year : undefined;
       const fdiPrevQuarter = data[1] ? data[1]["Quarter ID"] : undefined;
       const fdiPrevQuarterYear = data[4] ? data[4]["Quarter ID"] : undefined;
       return {
         fdiLatestQuarter,
         fdiLatestYear,
         fdiPrevQuarter,
-        fdiPrevQuarterYear,
+        fdiPrevQuarterYear
       };
     };
 
@@ -87,16 +87,15 @@ module.exports = function (app) {
           const parent = allData[id1];
           if (parent) {
             customId = parent["State ID"];
-            customName = parent["State"];
+            customName = parent.State;
           }
         }
 
         const customHierarchy = isMunicipality ? "State" : hierarchy1;
 
-        const ENOE_GEO = await ENOE_DATASET(
-          customHierarchy,
-          customId
-        ).catch(() => {});
+        const ENOE_GEO = await ENOE_DATASET(customHierarchy, customId).catch(
+          () => {}
+        );
         enoeLatestQuarter = ENOE_GEO.enoeLatestQuarter;
         enoePrevQuarter = ENOE_GEO.enoePrevQuarter;
         enoePrevYear = ENOE_GEO.enoePrevYear;
@@ -104,16 +103,14 @@ module.exports = function (app) {
         const covidParams = {
           cube: "gobmx_covid",
           drilldowns: "Updated Date",
-          measures: "Cases",
+          measures: "Cases"
           // [hierarchy1]: id1
         };
         const covidUpdated = await axios
-          .get(BASE_API, { params: covidParams })
-          .then((resp) => resp.data.data)
+          .get(BASE_API, {params: covidParams})
+          .then(resp => resp.data.data)
           .catch(catcher);
-        covidUpdated.sort(
-          (a, b) => b["Updated Date ID"] - a["Updated Date ID"]
-        );
+        covidUpdated.sort((a, b) => b["Updated Date ID"] - a["Updated Date ID"]);
         const covidLatestUpdated = covidUpdated[0]
           ? covidUpdated[0]["Updated Date ID"]
           : undefined;
@@ -121,8 +118,8 @@ module.exports = function (app) {
           hierarchy1 === "Nation"
             ? "coneval_gini_nat"
             : hierarchy1 === "State"
-            ? "coneval_gini_ent"
-            : "coneval_gini_mun";
+              ? "coneval_gini_ent"
+              : "coneval_gini_mun";
 
         return res.json({
           covidLatestUpdated,
@@ -130,8 +127,8 @@ module.exports = function (app) {
             hierarchy1 === "State"
               ? "gobmx_covid_stats_state"
               : hierarchy1 === "Nation"
-              ? "gobmx_covid_stats_nation"
-              : "gobmx_covid_stats_mun",
+                ? "gobmx_covid_stats_nation"
+                : "gobmx_covid_stats_mun",
           customSocialLagCube: `coneval_social_lag_${isState ? "ent" : "mun"}`,
           customGiniCube,
           customForeignTradeCube: isState
@@ -147,14 +144,14 @@ module.exports = function (app) {
           foreignTradeLatestMonth: 202005,
           foreignTradeLatestYear: 2019,
           fdiLatestQuarter: 20201,
-          fdiLatestYear: 2019,
+          fdiLatestYear: 2019
         });
 
       // Product profile
       case 11:
         return res.json({
           foreignTradeLatestYear: 2019,
-          foreignTradePrevYear: 2018,
+          foreignTradePrevYear: 2018
         });
 
       // Institution profile
@@ -163,32 +160,31 @@ module.exports = function (app) {
           cube: "anuies_enrollment",
           drilldowns: "Year",
           measures: "Students",
-          [hierarchy1]: id1,
+          [hierarchy1]: id1
         };
         const anuiesUpdated = await axios
-          .get(BASE_API, { params: anuiesParams })
-          .then((resp) => resp.data.data)
+          .get(BASE_API, {params: anuiesParams})
+          .then(resp => resp.data.data)
           .catch(catcher);
-        anuiesUpdated.sort((a, b) => b["Year"] - a["Year"]);
+        anuiesUpdated.sort((a, b) => b.Year - a.Year);
         const anuiesLatestYear = anuiesUpdated[0]
-          ? anuiesUpdated[0]["Year"]
+          ? anuiesUpdated[0].Year
           : undefined;
 
         const anuiesPrevYear = anuiesUpdated[1]
-          ? anuiesUpdated[1]["Year"]
+          ? anuiesUpdated[1].Year
           : undefined;
 
         return res.json({
           anuiesLatestYear,
-          anuiesPrevYear,
+          anuiesPrevYear
         });
 
       // Occupation profile
       case 28:
-        const ENOE_OCCUPATION = await ENOE_DATASET(
-          hierarchy1,
-          id1
-        ).catch(() => {});
+        const ENOE_OCCUPATION = await ENOE_DATASET(hierarchy1, id1).catch(
+          () => {}
+        );
         enoeLatestQuarter = ENOE_OCCUPATION.enoeLatestQuarter;
         enoePrevQuarter = ENOE_OCCUPATION.enoePrevQuarter;
         enoePrevYear = ENOE_OCCUPATION.enoePrevYear;
@@ -196,21 +192,20 @@ module.exports = function (app) {
         return res.json({
           enoeLatestQuarter,
           enoePrevQuarter,
-          enoePrevYear,
+          enoePrevYear
         });
 
       // Industry profile
       case 33:
-        const ENOE_INDUSTRY = await ENOE_DATASET(
-          hierarchy1,
-          id1
-        ).catch(() => {});
+        const ENOE_INDUSTRY = await ENOE_DATASET(hierarchy1, id1).catch(
+          () => {}
+        );
         const FDI_INDUSTRY = await FDI_DATASET(hierarchy1, id1).catch(() => {});
         const {
           fdiLatestQuarter,
           fdiLatestYear,
           fdiPrevQuarter,
-          fdiPrevQuarterYear,
+          fdiPrevQuarterYear
         } = FDI_INDUSTRY;
         enoeLatestQuarter = ENOE_INDUSTRY.enoeLatestQuarter;
         enoePrevQuarter = ENOE_INDUSTRY.enoePrevQuarter;
@@ -232,7 +227,7 @@ module.exports = function (app) {
           fdiLatestYear,
           fdiPrevQuarter,
           fdiPrevQuarterYear,
-          isDeepestLevel,
+          isDeepestLevel
         });
 
       default:
