53e88e1e01b08a38e40ed7f387373485155b71fe
nicoperezmundaca <nico.perezmundaca@gmail.com>
2020-08-28 10:39:51 -0400

    custom attributes update

--- api/customAttributes.js
+++ api/customAttributes.js
@@ -8,8 +8,8 @@ const catcher = error => {
   if (verbose) console.error("Custom Attribute Error:", error);
   return [];
 };
-module.exports = function(app) {
-  app.post("/api/cms/customAttributes/:pid", async(req, res) => {
+module.exports = function (app) {
+  app.post("/api/cms/customAttributes/:pid", async (req, res) => {
     const pid = req.params.pid * 1;
     const {cache} = app.settings;
     const {variables} = req.body;
@@ -24,7 +24,7 @@ module.exports = function(app) {
     } = variables;
 
     // ENOE: Shared customAttribute
-    const ENOE_DATASET = async(hierarchy, id) => {
+    const ENOE_DATASET = async (hierarchy, id) => {
       const params = {
         cube: "inegi_enoe",
         drilldowns: "Quarter",
@@ -44,7 +44,7 @@ module.exports = function(app) {
     };
 
     // FDI: Shared customAttribute
-    const FDI_DATASET = async(hierarchy, id) => {
+    const FDI_DATASET = async (hierarchy, id) => {
       const params = {
         cube: "economy_fdi",
         drilldowns: "Quarter",
@@ -149,9 +149,30 @@ module.exports = function(app) {
 
       // Product profile
       case 11:
+        const productParams = {
+          cube: "economy_foreign_trade_ent",
+          drilldowns: "Quarter",
+          measures: "Trade Value",
+          parents: "true"
+        };
+
+        const productQuaters = await axios.get(BASE_API, {params: productParams})
+          .then(resp => resp.data.data)
+          .catch(catcher);
+
+        const yearsInData = [...new Set(productQuaters.map(m => m.Year))];
+        yearsInData.sort((a, b) => b - a);
+
+        let tradeLatestYear = undefined;
+
+        for (const year of yearsInData) {
+          const quetersPerYear = productQuaters.filter(d => d.Year === year).length;
+          if (quetersPerYear === 4 && !tradeLatestYear) tradeLatestYear = year;
+        }
+
         return res.json({
-          foreignTradeLatestYear: 2019,
-          foreignTradePrevYear: 2018
+          foreignTradeLatestYear: tradeLatestYear,
+          foreignTradePrevYear: tradeLatestYear - 1
         });
 
       // Institution profile
