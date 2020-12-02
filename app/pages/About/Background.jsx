aeeb2eaa3f6d4f0d53f9096823d1a2ed1018797a
nicoperezmundaca <nico.perezmundaca@gmail.com>
2020-12-02 12:52:42 -0300

    minor change in about traduction

--- app/pages/About/Background.jsx
+++ app/pages/About/Background.jsx
@@ -7,14 +7,16 @@ import "./About.css";
 
 class Background extends Component {
   render() {
-    const {t, background} = this.props;
+    const {t, lng, background} = this.props;
+    const sub = {
+      es: "Está permitida la reproducción total o parcial de este video, por cualquier medio electrónico, para la difusión de DataMéxico",
+      en: "The total or partial reproduction of this video is allowed, by any electronic means, for the dissemination of DataMéxico"
+    }
     return (
       <div className="about-background">
         <h3>{t("About.Background")}</h3>
         <iframe className="about-video" src="https://www.youtube.com/embed/12N7aBzzGDc" frameBorder="0" allowFullScreen></iframe>
-        <sub>
-          Está permitida la reproducción total o parcial de este video, por cualquier medio electrónico, para la difusión de DataMéxico
-        </sub>
+        <sub>{sub[lng]}</sub>
         {background.map((d, i) =>
           <p key={i}>{d.Text}</p>
         )}
