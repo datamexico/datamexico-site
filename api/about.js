5e66d214f6e7e6efbb5c580e8a6cac8f4e194f77
Annie <annabaellaizm@gmail.com>
2021-04-23 17:13:04 -0400

    Change variables in ccs and use t in translations

--- api/about.js
+++ api/about.js
@@ -22,67 +22,67 @@ const VERSIONS = {
 	en: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=1810252159"
 }
 const CHALLENGES = {
-  es: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=180037722",
-  en: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=1222752537"
+	es: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=180037722",
+	en: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=1222752537"
 }
 const INFOAPI = {
-  es: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=2067915636",
-  en: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=1686248389"
+	es: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=2067915636",
+	en: "https://docs.google.com/spreadsheets/u/1/d/1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs/export?format=tsv&id=1al8qMBhGOjRU2rGLzetj_uPjE4jmNJ9MWl-hVOlQPPs&gid=1686248389"
 }
 
 module.exports = function (app) {
-  app.get(BASE_URL, async (req, res) => {
-	  const lng = req.params.lng;
-	try {
-	  await axios
-		.all([axios.get(BACKGROUND[lng]), axios.get(PRESS[lng]), axios.get(GLOSSARY[lng]), axios.get(LEGAL[lng]), axios.get(VERSIONS[lng]), axios.get(CHALLENGES[lng]), axios.get(INFOAPI[lng])])
-		.then(axios.spread((...resp) => {
-		  const dictionary = {
-			0: {Texto: "Text"},
-			1: {Fecha: "Date", Plataforma: "Platform", Medio: "Media", Titular: "Title", Descripción: "Text", Fotografía: "Picture", URL: "URL"},
-			2: {Concepto: "Concept", Descripción: "Description"},
-			3: {Título: "Title", Descripción: "Description"},
-			4: {Versión: "Version", Caracteristicas: "Features", Descripción: "Description"},
-			5: {Desafío: "Challenge", Descripción: "Description", Lugar: "Position", Proyectos: "Project", Explicación: "Explanation", URL: "URL"},
-      6: {Texto: "Text", Concepto: "Concept", Descripción: "Description", Link: "Link"},
-		  }
-		  const respData = resp.map(d => d.data);
-		  const csvData = respData.map((respItem, respIndex) => {
-			const csv = respItem.split("\r\n").map(d => d.split("\t"));
-			const csvHeader = csv[0];
-			const csvDictionary = dictionary[respIndex];
-			const data = csv.slice(1).reduce((all, d) => {
-			  const item = {};
-			  respIndex === 2 && (item["Initial"] = d[0].charAt(0).toUpperCase()); //Initials for glossary
-			  csvHeader.forEach((h, i) => {
-				item[csvDictionary[h]] = d[i] !== "" ? d[i] : null;
-			  });
-			  all.push(item);
-			  return all;
-			}, []);
-			return data;
-		  });
+	app.get(BASE_URL, async (req, res) => {
+		const lng = req.params.lng;
+		try {
+			await axios
+				.all([axios.get(BACKGROUND[lng]), axios.get(PRESS[lng]), axios.get(GLOSSARY[lng]), axios.get(LEGAL[lng]), axios.get(VERSIONS[lng]), axios.get(CHALLENGES[lng]), axios.get(INFOAPI[lng])])
+				.then(axios.spread((...resp) => {
+					const dictionary = {
+						0: {Texto: "Text"},
+						1: {Fecha: "Date", Plataforma: "Platform", Medio: "Media", Titular: "Title", Descripción: "Text", Fotografía: "Picture", URL: "URL"},
+						2: {Concepto: "Concept", Descripción: "Description"},
+						3: {Título: "Title", Descripción: "Description"},
+						4: {Versión: "Version", Caracteristicas: "Features", Descripción: "Description"},
+						5: {Desafío: "Challenge", Descripción: "Description", Lugar: "Position", Proyectos: "Project", Explicación: "Explanation", URL: "URL"},
+						6: {Texto: "Text", Concepto: "Concept", Descripción: "Description", Link: "Link"},
+					}
+					const respData = resp.map(d => d.data);
+					const csvData = respData.map((respItem, respIndex) => {
+						const csv = respItem.split("\r\n").map(d => d.split("\t"));
+						const csvHeader = csv[0];
+						const csvDictionary = dictionary[respIndex];
+						const data = csv.slice(1).reduce((all, d) => {
+							const item = {};
+							respIndex === 2 && (item["Initial"] = d[0].charAt(0).toUpperCase()); //Initials for glossary
+							csvHeader.forEach((h, i) => {
+								item[csvDictionary[h]] = d[i] !== "" ? d[i] : null;
+							});
+							all.push(item);
+							return all;
+						}, []);
+						return data;
+					});
 
-		  csvData[1].forEach(d => {
-			const date = d.Date.split("-");
-			const dateID = date[2]+date[1]+date[0];
-			return d["Date ID"] = dateID
-		  })
+					csvData[1].forEach(d => {
+						const date = d.Date.split("-");
+						const dateID = date[2] + date[1] + date[0];
+						return d["Date ID"] = dateID
+					})
 
-		  res.json({
-			background: csvData[0],
-			press: csvData[1],
-			glossary: csvData[2],
-			terms: csvData[3],
-			version: csvData[4],
-			challenge: csvData[5],
-      infoapi: csvData[6]
-		  });
-		}));
-	} catch (e) {
-	  console.log(e);
-	  return [];
-	}
-  });
+					res.json({
+						background: csvData[0],
+						press: csvData[1],
+						glossary: csvData[2],
+						terms: csvData[3],
+						version: csvData[4],
+						challenge: csvData[5],
+						infoapi: csvData[6]
+					});
+				}));
+		} catch (e) {
+			console.log(e);
+			return [];
+		}
+	});
 
 };
--- app/pages/About/Infoapi.jsx
+++ app/pages/About/Infoapi.jsx
@@ -7,21 +7,13 @@ import "./About.css";
 
 class Infoapi extends Component {
   render() {
-    const {t, lng, infoapi} = this.props;
-    const intro1 = {
-      es: "Para acceder de forma rápida y dinámica a los datos disponibles en dataMéxico se utiliza Tesseract UI, un explorador dinámico de API’s disponible en el siguiente enlace: ",
-      en: "To quickly and dynamically access the data available in dataMéxico, Tesseract UI is used, a dynamic API's explorer available al the following link: "
-    }
-    const intro2 = {
-      es: "Tesseract UI le permite visualizar los datos consultados en su interfaz y le otoga la posibilidad de descargar los datos para sus propios análisis en formato CSV, JSON Tidy y JSON Arrays.",
-      en: "Tesseract UI allows you to view the queried data in its interface and gives you the possibility to download the data for your own analysis in CSV, JSON Tidy and JSON Arrays formats."
-    }
+    const {t, infoapi} = this.props;
 
     return (
       <div className="about-infoapi">
         <h3>{t("About.Access to data")}</h3>
-        <p>{intro1[lng]} <a href="https://api.datamexico.org/ui" target="_blank" rel="noopener noreferrer">api.datamexico.org/ui</a></p>
-        <p>{intro2[lng]} </p>
+        <p>{t("About.IntroAPI1")} <a href="https://api.datamexico.org/ui" target="_blank" rel="noopener noreferrer">api.datamexico.org/ui</a></p>
+        <p>{t("About.IntroAPI2")} </p>
 
           {infoapi.length > 0 && infoapi.map(d => (
             <div className="press-news">
